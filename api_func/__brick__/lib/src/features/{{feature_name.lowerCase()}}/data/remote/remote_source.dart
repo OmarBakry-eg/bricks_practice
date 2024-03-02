cd280ccc4e5d9d256fce16c37aabcb4ab5e90712
OmarBakry-eg <omarbakry202020@gmail.com>
2024-03-02 02:39:18 +0200

    adding bodyIsList, bodyIsListOfMaps, hasParameters, parameters

--- api_func/__brick__/lib/src/features/{{feature_name.lowerCase()}}/data/remote/remote_source.dart
+++ api_func/__brick__/lib/src/features/{{feature_name.lowerCase()}}/data/remote/remote_source.dart
@@ -1,17 +1,31 @@
   
   
   @override
-  Future<{{model_name}}> {{func_name}} ({{#token}}String? token{{/token}}) async {
+  Future<{{model_name}}> {{func_name}} ({{#token}}String? token{{/token}} {{#hasParameters}}, { {{#parameters}}
+    {{#isRequired}} required {{/isRequired}} {{key}} {{^isRequired}}?{{/isRequired}} {{value}},
+    {{/parameters}}
+  } {{/hasParameters}}) async {
       {{#token}} 
     if (token == null) {
       throw const EmptyCacheException(message: 'Missing token');
     }
     {{/token}} 
-    Result result = await _dio.get("{{api_url}}", {{#token}}  token {{/token}} , isLoading: false, {{^emptyBody}} body: {
+    Result result = await _dio.get("{{api_url}}", {{#token}} token {{/token}} , isLoading: false, {{^emptyBody}} body: {{#bodyIsList}} [
+    {{^bodyIsListOfMaps}}  {
       {{#body}} 
       "{{key}}" : {{value}},
       {{/body}}
-    } {{/emptyBody}});
+    }
+    {{/bodyIsListOfMaps}}
+    {{#bodyIsListOfMaps}} {{#body}} {
+      "{{key}}" : {{value}},
+    },
+      {{/body}}{{/bodyIsListOfMaps}} 
+    ] {{/bodyIsList}} {{^bodyIsList}} {
+      {{#body}} 
+      "{{key}}" : {{value}},
+      {{/body}}
+    } {{/bodyIsList}} {{/emptyBody}});
     if (result is SuccessState) {
       return {{model_name}}.fromJson(result.value);
     } else if (result is ErrorState) {
